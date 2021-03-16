45f4e837b360563775177d98177a1eee7500a2a9
sarthakchauhan <schauh04>
2021-03-16 19:55:29 +0530

    added ScreenTemplate.java for GUI

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -2,6 +2,10 @@ package ui;
 
 public class Main {
     public static void main(String[] args) {
-        new MyApp();
+        //new MyApp();
+        String[] options = {"Back",
+                "Show Products", "Search Products", "Add to Wishlist",
+                "Add to Cart"};
+        new ScreenTemplate("Home", options);
     }
 }
